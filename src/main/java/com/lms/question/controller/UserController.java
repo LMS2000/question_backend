00666a620c633cfe8a3645655d29337bf21a36a1
LMS <956287248@qq.com>
2023-07-06 19:34:59 +0800

    50%

--- src/main/java/com/lms/question/controller/UserController.java
+++ src/main/java/com/lms/question/controller/UserController.java
@@ -43,6 +43,7 @@ public class UserController {
     }
 
 
+    @PostMapping("/register")
     public Boolean  userRegister(@Validated @RequestBody RegisterUserDto registerUserDto){
         return userService.registerUser(registerUserDto);
     }
